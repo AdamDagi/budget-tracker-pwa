8044289a8b0f368441eb538e82ef4000e7127b37
AdamDagi <mugutdinovshiha02@gmail.com>
2022-02-04 15:52:54 -0500

    working on serwise worker

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,42 @@
+var db;
+var trx;
+
 self.addEventListener('install', event => {
     console.log("install");
+
+    let openRequest = indexedDB.open("BudgetTrackerDB", 1);
+
+    openRequest.onerror = function() {
+        console.error("Error", openRequest.error);
+    };
+      
+    openRequest.onsuccess = function() {
+        console.log('Success');
+    };
+
+    openRequest.onupgradeneeded = function(event) {
+        db = event.target.result;
+        if (!db.objectStoreNames.contains('transaction')) {
+            trx = db.createObjectStore('transaction', {keyPath: 'id'});
+        };
+    };
+    
 });
 
 self.addEventListener('active', event => {
+    event.waitUntil(
+        createDB()
+    );
     console.log("activ");
 });
+
+function createDB() {
+    idb.open('products', 1, function(upgradeDB) {
+      var store = upgradeDB.createObjectStore('beverages', {
+        keyPath: 'id'
+      });
+      store.put({id: 123, name: 'coke', price: 10.99, quantity: 200});
+      store.put({id: 321, name: 'pepsi', price: 8.99, quantity: 100});
+      store.put({id: 222, name: 'water', price: 11.99, quantity: 300});
+    });
+}
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budgetTrackerPWA";
 
 const app = express();
 
