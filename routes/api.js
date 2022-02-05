1d63cc9acc8abd17f9b7e971aaeb3fe12a528c27
AdamDagi <mugutdinovshiha02@gmail.com>
2022-02-05 11:29:15 -0500

    added indexedDB

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
 router.post("/api/transaction", ({body}, res) => {
+  console.log("test send data");
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
